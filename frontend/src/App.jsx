0b55b57a664180f364027f4848cb00651980e717
dharanidh75 <dharanidh777.com>
2026-05-22 21:05:44 +0530

    logo bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -125,7 +125,7 @@ function Home() {
     <div className="app">
       {toastEl}
       <div className="nav">
-        <img src="public/favicon.png" alt="logo" 
+        <img src="/favicon.png" alt="logo" 
         style={{ width: 50, height: 50 }}/>
         <h1 className="title">ResHub</h1>
         <span className="nav-username">👋 {getUsername()}</span>
